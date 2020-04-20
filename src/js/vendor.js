07ba00456b2b5e208099d7c8b596e787125a68f9
johan Vanelderen <vanelderen.johan@telenet.be>
2020-04-20 06:59:29 +0200

    added fontawesome

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,4 +1,9 @@
 console.log('HI VAN VENDOR!!');
 // IMPORT VENDOR JS ZOALS BOOTSTRAP ENZ
+//import '@fortawesome/fontawesome-free/js/all';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 

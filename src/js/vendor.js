b0513fab007b8836e59c23d569a7d8bb2912fa0a
Johan Vanelderen <johanvanelderen@MacBook-Air-van-Johan.local>
2020-05-09 18:55:04 +0200

    html ready

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -6,4 +6,7 @@ import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 
+import barba from '@barba/core';
+
+
 

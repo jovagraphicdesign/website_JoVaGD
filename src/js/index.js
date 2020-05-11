a71e4404e5d31d831229ca4cd2e1ccd5896d97ab
johan Vanelderen <vanelderen.johan@telenet.be>
2020-05-11 09:04:56 +0200

    scss

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
 console.log('HALLO VAN WEBPACK!!');
 import "../scss/main.scss";
 
+function menuToggle(){
+    const nav = document.querySelector('.menu_overlay');
+    nav.classList.toggle('active');
+}

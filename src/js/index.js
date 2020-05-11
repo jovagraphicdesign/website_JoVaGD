e7244c5c3497af05f295d77c1b7d6ca0cbff44a3
Johan Vanelderen <johanvanelderen@MacBook-Air-van-Johan.local>
2020-05-11 17:37:58 +0200

    menu afgewerkt

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,20 @@
 console.log('HALLO VAN WEBPACK!!');
 import "../scss/main.scss";
 
-function menuToggle(){
-    const nav = document.querySelector('.menu_overlay');
-    nav.classList.toggle('active');
-}
+const MenuBtn  = document.querySelector('.toggleIcon');
+const nav = document.querySelector('.menu_overlay');
+let menuOpen = false;
+
+MenuBtn.addEventListener('click', ()=>{
+    console.log('clicked');
+    if(!menuOpen){
+        MenuBtn.classList.add('open');
+        nav.classList.add('active');
+        menuOpen = true;
+    }else{
+        MenuBtn.classList.remove('open');
+        nav.classList.remove('active');
+        menuOpen = false;
+    }
+    //nav.classList.toggle("active");
+});

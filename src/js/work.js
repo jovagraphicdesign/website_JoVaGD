73ae4a2b6395651abfa7e4a256c1f85538073596
Johan Vanelderen <johanvanelderen@MacBook-Air-van-Johan.local>
2020-05-17 11:48:14 +0200

    work on work list

--- src/js/work.js
+++ src/js/work.js
@@ -1,1 +1,12 @@
 console.log('hi motherfucker for about');
+const workList = document.querySelectorAll('.work-list>li');
+console.log(workList);
+
+workList.forEach(
+    function(listItem){
+        listItem.addEventListener('mouseover', (e) => {
+            console.log(e.target.getAttribute('data-project'));
+             
+        })
+    }
+)

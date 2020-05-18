7f3e30c7d7770e89742eefec1e238274a5703654
johan Vanelderen <vanelderen.johan@telenet.be>
2020-05-18 14:57:24 +0200

    js work section

--- src/js/work.js
+++ src/js/work.js
@@ -1,12 +1,29 @@
 console.log('hi motherfucker for about');
-const workList = document.querySelectorAll('.work-list>li');
+const workList = document.querySelectorAll('.work-list>li>a');
+const workImgContColor = "linear-gradient(0deg, rgba(19, 38, 64, 0.7), rgba(19, 38, 64, 0.7))";
+const workContainer = document.querySelector(".img-container");
+const Urlproject_1 = "../assets/woman.jpg"
 console.log(workList);
 
 workList.forEach(
-    function(listItem){
+    function(listItem, index){
+        console.log(workList);
+        
         listItem.addEventListener('mouseover', (e) => {
-            console.log(e.target.getAttribute('data-project'));
-             
+            ProjectDetails(e.target.getAttribute('data-project'),index);
+        });
+        listItem.addEventListener('mouseout', () =>{
+            workContainer.style.background = workImgContColor;
         })
     }
-)
+);
+
+function ProjectDetails (dataProject, index){
+    
+    let item = index +1
+    console.log(dataProject, item) ;
+    //let urlStr = "Url"+dataProject;
+    //console.log(urlStr);
+    workContainer.style.background = 'red';
+    
+}

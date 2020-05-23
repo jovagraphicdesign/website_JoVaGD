2ef37a9b4c32cff6fe3f3e38c5599f2ebcb07f45
Johan Vanelderen <johanvanelderen@MacBook-Air-van-Johan.local>
2020-05-23 14:01:34 +0200

    add day en month js

--- src/js/home.js
+++ src/js/home.js
@@ -3,6 +3,14 @@ const ctaAbout = document.querySelector('.cta_about');
 let day = document.querySelector('.day');
 let month = document.querySelector('.month');
 
+
+let date =  new Date();
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const monthIndex = (date.getMonth()-1);
+
+
+
+
  
 ctaAbout.addEventListener('mouseover', ()=>{
     ctaTouch.style.background='#3FBF9B';
@@ -20,3 +28,6 @@ ctaTouch.addEventListener('mouseout', () =>{
     ctaAbout.style.background='#3FBF9B';
     ctaTouch.style.background='#4BF2B5';
 });
+
+day.innerHTML = date.getDate();
+month.innerHTML = months[monthIndex]
